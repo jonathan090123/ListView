5451cf821417e4521c32cd41c6e7f69d85afce63
ASUS <hantububu5758@gmail.com>
2024-11-06 18:51:13 +0700

    FINAL

--- app/src/main/java/uts/c14220035/listview/MainActivity.kt
+++ app/src/main/java/uts/c14220035/listview/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     var data = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        data.addAll(listOf("1","2","3","4","5"))
+        data.addAll(listOf("1", "2", "3", "4", "5"))
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,13 +25,27 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val lvAdapter : ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
-        val _lv1 : ListView = findViewById(R.id.lvl1)
+
+        val lvAdapter: ArrayAdapter<String> =
+            ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
+        val _lv1: ListView = findViewById(R.id.lvl1)
         _lv1.adapter = lvAdapter
-        val _btnTambah : Button = findViewById(R.id.btnTambah)
+
+        val _btnTambah: Button = findViewById(R.id.btnTambah)
         _btnTambah.setOnClickListener {
-            var dtAkhir = Integer.parseInt(data.get(data.size-1)) + 1
+            val dtAkhir = Integer.parseInt(data[data.size - 1]) + 1
             data.add(dtAkhir.toString())
+            lvAdapter.notifyDataSetChanged()
+            val setOnItemClickListener = _lv1.setOnItemClickListener { parent, view, position, id ->
+                Toast.makeText(
+                    this,
+                    "${data[position]}", Toast.LENGTH_SHORT).show()
+            }
+            val _btnHapus : Button = findViewById<Button>(R.id.btnHapus)
+            _btnHapus.setOnClickListener {
+                data.removeFirst()
+                lvAdapter.notifyDataSetChanged()
+            }
         }
     }
 }
